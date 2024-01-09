c1f0b829d29134b7f2e607d7c4469cf8d6a89160
AnanyaSinha2717 <ananyasinha2717@gmail.com>
2024-01-09 20:46:55 +0530

    max area of rectangle with longest diagonal

---
+++ leet_3000.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int d, max_d = 0, area = 0;
+
+    int dimensions[2][2] = {{2, 3}, {8, 8}};
+    for (int i = 0; i < 2; i++)
+    {
+        int l = dimensions[i][0];
+        int b = dimensions[i][1];
+        d = l * l + b * b;
+        cout << "dia of rec" << i << " = " << d << endl;
+
+        if (d > max_d || (d == max_d && l * b > area))
+        {
+            max_d = d;
+            area = l * b;
+        }
+    }
+    cout << "area of max diag rectangle = " << area << endl;
+    return 0;
+}
